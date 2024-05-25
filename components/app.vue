d2a87898377177d773e98144cc4ec3c6ad3a3f10
cc <203717588@qq.com>
2024-05-25 20:36:25 +0800

    5-25一次性提交

---
+++ components/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-app">
+    <h2>{{ msg }}</h2>
+    <!--插入组件：引入的组件标签，和import中的名称保持一致-->
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+
+// vue项目必须安装node
+
+// 引入Home组件
+import Home from "./home";
+
+// 暴露一个组件，方便外部读取以及访问
+export default {
+  name: "App",
+  // 注册引入的组件
+  components: {Home},
+  data() {
+    return {
+      msg: '这是App页面!'
+    }
+  }
+}
+
+</script>
---
+++ components/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-home">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<!--template(模板)标签：其html内容在页面中不会显示，默认设置了display:none属性-->
+
+<script>
+
+// 暴露一个组件，以让其它组件引入(方便外部读取以及访问)
+export default {
+  name: "Home",
+  data() {
+    return {
+      msg: '这是Home页面!'
+    }
+  }
+}
+
+</script>
